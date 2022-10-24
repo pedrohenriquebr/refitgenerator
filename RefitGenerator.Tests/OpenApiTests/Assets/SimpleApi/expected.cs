6aaa23c1599b95cff3880fb2c29d73865a612f80
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-10-24 14:56:09 -0300

    test: add SimpleApi unit test

---
+++ RefitGenerator.Tests/OpenApiTests/Assets/SimpleApi/expected.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Refit;
+
+public interface ISimpleOpenApiOverviewService
+{
+    [Get]
+    public Task<ListVersionsv2Response> ListVersionsv2();
+
+    [Get("/v2")]
+    public Task<GetVersionDetailsv2Response> GetVersionDetailsv2();
+}
+
+public class ListVersionsv2Response
+{
+    public List<VersionsResponse> Versions { get; set; }
+}
+
+public class VersionsResponse
+{
+    public string Status { get; set; }
+    public DateTime Updated { get; set; }
+    public string Id { get; set; }
+    public List<LinksResponse> Links { get; set; }
+}
+
+public class LinksResponse
+{
+    public string Href { get; set; }
+    public string Rel { get; set; }
+}
