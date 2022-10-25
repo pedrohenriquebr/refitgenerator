3061b5ca5e253a87d1f4b62bf5bcd488a79d8665
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-10-24 22:15:02 -0300

    refactor: refactor OpenApiParser test

--- RefitGenerator.Tests/OpenApiTests/OpenApiParserTests.cs
+++ RefitGenerator.Tests/OpenApiTests/OpenApiParserTests.cs
@@ -1,12 +1,6 @@
-﻿using Microsoft.OpenApi.Models;
-using Microsoft.OpenApi.Readers;
-using Microsoft.OpenApi.Readers.Interface;
-using Microsoft.VisualStudio.TestPlatform.Utilities;
+﻿using Microsoft.OpenApi.Readers;
 using RefitGenerator.Core;
 using RefitGenerator.Factories;
-using RefitGenerator.Util;
-using System;
-using System.Runtime.Serialization;
 
 namespace RefitGenerator.Tests.OpenApiTests;
 
@@ -18,8 +12,7 @@ public class OpenApiParserTests
     [Fact]
     public void Should_Generate_For_OpenApi()
     {
-        var formatter = CreateSourceFormatter;
-        var factory = CreateFactory(formatter);
+        var factory = CreateFactory();
         var openApiReader = CreateOpenApiReader();
         var (input, expected) = LoadTestAssets("SimpleApi");
         string output = new DefaultOpenApiConverter(openApiReader, factory)
@@ -30,8 +23,8 @@ public class OpenApiParserTests
 
     public static ISourceFormatterProvider CreateSourceFormatter()
         => new DefaultSourceFormatter(EOF, INDENT);
-    public static MethodAlgebraGenerator CreateFactory(Func<ISourceFormatterProvider> func)
-        => new MethodAlgebraGenerator(func());
+    public static MethodAlgebraGenerator CreateFactory()
+        => new MethodAlgebraGenerator(CreateSourceFormatter());
 
     public static OpenApiStringReader CreateOpenApiReader()
         => new OpenApiStringReader();
@@ -55,83 +48,3 @@ public class OpenApiParserTests
         return matrix[testName];
     }
 }
-
-
-public interface IOpenApiConverter
-{
-    public string Convert(string openApiJson);
-    public string CreateMethodName(string operationId);
-    public string CreateMethodResponseName(string methodName);
-    public IAttributeBehavior CreateMethodAttribute(OperationType key);
-    public IStatementBehavior CreateInterfaceMethod(KeyValuePair<OperationType, OpenApiOperation> operation);
-}
-
-
-public class DefaultOpenApiConverter : IOpenApiConverter
-{
-    private readonly IOpenApiReader<string, OpenApiDiagnostic> openApiReader;
-    private readonly MethodAlgebraGenerator factory;
-    public DefaultOpenApiConverter(
-        IOpenApiReader<string, OpenApiDiagnostic> openApiReader,
-        MethodAlgebraGenerator methodAlgebraGenerator)
-    {
-        this.openApiReader = openApiReader;
-        this.factory = methodAlgebraGenerator;
-    }
-
-    public string CreateMethodName(string operationId)
-        => operationId[0].ToString().ToUpper() + operationId[1..].ToString();
-    private string Convert(OpenApiDocument openApiDoc)
-    {
-        return factory
-            .Block(factory.Compose(
-                            factory.UsingNamespace("System"),
-                            factory.UsingNamespace("Refit")),
-                        
-                        factory.Interface(
-                            name: "ISimpleOpenApiOverviewService",
-                            body: factory.Compose(
-                                openApiDoc.Paths.Keys.SelectMany(pathKey =>
-                                    openApiDoc.Paths[pathKey].Operations
-                                        .Select(operation => CreateInterfaceMethod(operation))
-                                    ).ToArray()
-                                )
-                        )
-                        )
-            .Generate();
-    }
-
-    public IAttributeBehavior CreateMethodAttribute(OperationType key)
-        => key switch
-        {
-            >= OperationType.Get and <= OperationType.Delete => factory.Attribute(key.ToString()),
-            _ => throw new ArgumentException("OperationType not supported!")
-        };
-
-    public string Convert(string openApiJson)
-    {
-        var openApiDoc = openApiReader.Read(openApiJson, out var diagnostic);
-
-        if (factory.Fomatter is null)
-            throw new Exception("SourceFormatter is null");
-
-        return factory.Fomatter.FormatCode(Convert(openApiDoc));
-    }
-
-    public string CreateMethodResponseName(string methodName)
-        => methodName + "Response";
-
-    public IStatementBehavior CreateInterfaceMethod(KeyValuePair<OperationType, OpenApiOperation> operation)
-    {
-        var methodName = CreateMethodName(operation.Value.OperationId);
-        var methodResponseTypeName = CreateMethodResponseName(methodName);
-        IAttributeBehavior attribute = CreateMethodAttribute(operation.Key);
-        return factory.InterfaceMethod(
-                    name: methodName,
-                    returnType: factory.Type($"Task<{methodResponseTypeName}>"),
-                    modifiers: new[] { factory.Public() },
-                    attributes: new[] { attribute }
-        );
-    }
-}
-
