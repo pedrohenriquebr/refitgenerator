68d725d2975406e5883181c8df1ff68623f8b674
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-10-25 22:49:20 -0300

    feat: add models

---
+++ RefitGenerator/Converter/Models/ClassModel.cs
@@ -0,0 +1,7 @@
+﻿namespace RefitGenerator.Converter.Models;
+
+public sealed record ClassModel
+{
+    public string Name { get; init; }
+    public List<ClassPropModel> Props { get; init; } = new List<ClassPropModel>();
+}
---
+++ RefitGenerator/Converter/Models/ClassPropModel.cs
@@ -0,0 +1,7 @@
+﻿namespace RefitGenerator.Converter.Models;
+
+public sealed record ClassPropModel
+{
+    public string Name { get; init; }
+    public string Type { get; init; }
+}
